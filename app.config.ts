fd525287d3e42aa9f2fe4faf5c90ef946e6f50ae
Benjamin Canac <canacb1@gmail.com>
2023-08-04 19:04:09 +0200

    with module

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 export default defineAppConfig({
   ui: {
-    color: 'red'
+    color: 'black'
   }
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    'my-module'
+  ],
+  vite: {
+    server: {
+      fs: {
+        // Allow serving files from one level up to the project root
+        allow: ['..'],
+      },
+    },
+  },
+
+
 })
